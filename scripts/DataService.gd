ccf4542132eb58450b76ccc28626ee9f10dbcddc
dipi <dipi.evil@gmail.com>
2021-08-31 11:17:27 -0300

    Add github actions

--- scripts/DataService.gd
+++ scripts/DataService.gd
@@ -2,16 +2,18 @@
 var file_path = "user://"
 
 func save_all():
-	save_file(load_data("player"),"player")
-	save_file(load_data("theme"),"theme")
-	save_file(load_data("company"),"company")
+	pass
+	#TO-DO: save remotely
+	#save_file(load_data("player"),get_file_name_by_resource("player"))
+	#save_file(load_data("theme"),get_file_name_by_resource("theme"))
+	#save_file(load_data("company"),get_file_name_by_resource("company"))
 
 func save_data(jsonData,resource_name):
-	var file_name = str(file_path) + "DATA_" + str(resource_name) + ".sav"
+	var file_name = get_file_name_by_resource(resource_name)
 	save_file(jsonData,file_name)
 
 func load_data(resource_name):
-	var file_name = str(file_path) + "DATA_" + str(resource_name) + ".sav"
+	var file_name = get_file_name_by_resource(resource_name)
 	return load_file(file_name)
 
 func save_file(data,file_name):
@@ -26,3 +28,6 @@ func load_file(file_name):
 	var content = file.get_as_text()	
 	file.close()
 	return parse_json(JSON.parse(content).result)
+
+func get_file_name_by_resource(resource_name):
+	return str(file_path) + "DATA_" + str(resource_name) + ".sav"
